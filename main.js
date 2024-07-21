0100eef6a89bbe8abdac1e7119edb2f2f8874314
Daniil Osipenko <daniilosipenko@coherentsolutions.com>
2024-07-21 12:36:12 +0300

    feat: add canvas

---
+++ main.js
@@ -0,0 +1,53 @@
+const { app, BrowserWindow } = require('electron')
+const url = require("url");
+const path = require("path");
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/game-web/browser/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// app.on('activate', function () {
+//   if (mainWindow === null) createWindow()
+// })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CanvasBoxComponent } from "./components/canvas-box/canvas-box.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: CanvasBoxComponent,
+  }
+];
